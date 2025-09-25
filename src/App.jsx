b5fe0229c2019f0868554d80199f4e211f797c57
cauamatelli <cauajanizelo@gmail.com>
2025-09-25 12:21:05 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import './App.css'
 import ferrari from './assets/f40.jpg'
-import ferrari2 from './assets/458.lnk'
-import ferrari3 from './assets/ferrari1.lnk'
-import LogoPuma from './assets/logo-puma-branco.png'
+import ferrari2 from './assets/ferrariss.jpg'
+import ferrari3 from './assets/italia.jpg'
+import LogoPuma from './assets/logof.png'
 import LogoInsta from './assets/instagram.svg'
 import LogoDiscord from './assets/discord.svg'
 import LogoWhats from './assets/whatsapp.svg'
@@ -56,10 +56,10 @@ function App() {
           <a href="#" className="link-card">
             <div className="card">
               <img src={ferrari} alt="" className="img-card"/>
-              <h2>Ferrari 488</h2>
-              <p className="desc">Formstrip bordado e uma gola acolchoada exagerada, ele mistura herança e streetwear moderno.</p>
+              <h2>Ferrari F40</h2>
+              <p className="desc">A Ferrari F40 possui motor V8 biturbo de 2.9 litros com 478 cavalos de potência.</p>
               <p className="classificacao"> <span>★ ★ ★ ★ ☆</span> (479)</p>
-              <p className="preco">R$ 599,99</p>
+              <p className="preco">R$ 5.000.000</p>
               <p className="off">- 5%</p>
             </div>
           </a>
@@ -68,11 +68,11 @@ function App() {
           <a href="#" className="link-card">
             <div className="card">
               <img src={ferrari2} alt="" className="img-card"/>
-              <h2>Tênis Suede XL Alien</h2>
-              <p className="desc">Formstrip bordado e uma gola acolchoada exagerada, ele mistura herança e streetwear moderno.</p>
-              <p className="classificacao"> <span>★ ★ ★ ★ ☆</span> (479)</p>
-              <p className="preco">R$ 599,99</p>
-              <p className="off">- 5%</p>
+              <h2>Ferrari 488</h2>
+              <p className="desc">A Ferrari 488 possui motor V8 biturbo de 3.9 litros com 670 cavalos de potência.</p>
+              <p className="classificacao"> <span>★ ★ ★ ★ ☆</span> (3079)</p>
+              <p className="preco">R$ 3.500.000</p>
+              <p className="off">- 7%</p>
             </div>
           </a>
 
@@ -80,18 +80,18 @@ function App() {
           <a href="#" className="link-card">
             <div className="card">
               <img src={ferrari3} alt="" className="img-card"/>
-              <h2>Tênis Suede XL Alien</h2>
-              <p className="desc">Formstrip bordado e uma gola acolchoada exagerada, ele mistura herança e streetwear moderno.</p>
-              <p className="classificacao"> <span>★ ★ ★ ★ ☆</span> (479)</p>
-              <p className="preco">R$ 599,99</p>
-              <p className="off">- 5%</p>
+              <h2>Ferrari 458 Italia</h2>
+              <p className="desc">A Ferrari 458 possui motor V8 aspirado de 4.5 litros com 570 cavalos de potência.</p>
+              <p className="classificacao"> <span>★ ★ ★ ★ ☆</span> (2679)</p>
+              <p className="preco">R$ 2.700.700</p>
+              <p className="off">- 35%</p>
             </div>
           </a>
 
         </section>
         <section className="destaque">
 
-          <iframe src="https://www.youtube.com/embed/p7cDN1ofIw0?autoplay=1&mute=1&controls=0&loop=1&playlist=p7cDN1ofIw0&modestbranding=1&showinfo=0" frameborder="0" allow="autoplay; fullscreen"></iframe>
+          <iframe src="https://www.youtube.com/embed/sa7MkxTzC9A?autoplay=1&mute=1&controls=0&loop=1&playlist=sa7MkxTzC9A&modestbranding=1&showinfo=0" frameborder="0" allow="autoplay; fullscreen"></iframe>
 
         </section>
 
@@ -118,7 +118,7 @@ function App() {
           </div>
 
           <p className="txt-footer">
-            PUMA SPORTS LTDA CNPJ 05.406.034/0023-00 Inscrição Estadual 278.205.224.119 Endereço: Rua Passadena 104 - Parque Industrial, Cotia - SP CEP: 06715864.
+            FERRARI SPORTS LTDA CNPJ 05.406.034/0023-00 Inscrição Estadual 278.205.224.119 Endereço: Rua Passadena 104 - Parque Industrial, Cotia - SP CEP: 06715864.
           </p>
 
         </footer>
